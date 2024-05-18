94ecec784d221900ccd3db612b8d6207a8dc5990
Schkimansky <ahmadchawla1432@gmail.com>
2024-05-18 22:41:12 +0500

    Remove speed conversion.
    it was pretty faulty and buggy + nobody is going to use it honestly

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-__all__ = ['time', 'frequency', 'ohm', 'volt', 'data', 'concentration', 'angle', 'ampere', 'distance', 'weight', 'volume', 'liquid', 'area', 'pressure', 'energy', 'power', 'speed', 'temperature']
+__all__ = ['time', 'frequency', 'ohm', 'volt', 'data', 'concentration', 'angle', 'ampere', 'distance', 'weight', 'volume', 'liquid', 'area', 'pressure', 'energy', 'power', 'temperature']
 
-from .main import time, frequency, ohm, volt, data, concentration, angle, ampere, distance, weight, volume, liquid, area, pressure, energy, power, speed, temperature
+from .main import time, frequency, ohm, volt, data, concentration, angle, ampere, distance, weight, volume, liquid, area, pressure, energy, power, temperature
 
-_ = time, frequency, ohm, volt, data, concentration, angle, ampere, distance, weight, volume, liquid, area, pressure, energy, power, speed, temperature
+_ = time, frequency, ohm, volt, data, concentration, angle, ampere, distance, weight, volume, liquid, area, pressure, energy, power, temperature
